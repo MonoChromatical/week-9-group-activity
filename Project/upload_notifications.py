1f94d35c1fe2cfe27a2bd0158422c5c514812258
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-04-21 15:08:01 +0800

    Camerons task

--- Project/upload_notifications.py
+++ Project/upload_notifications.py
@@ -1,1 +1,17 @@
-#Cameron
+class UploadNotifier:
+    def __init__(self):
+        self.subscribers = []
+
+    def subscribe(self, func):
+        self.subscribers.append(func)
+
+    def notify(self, document_name):
+        for func in self.subscribers:
+            func(document_name)
+
+
+def email_notification(document_name):
+    print(f"[EMAIL] {document_name} uploaded")
+
+def dashboard_notification(document_name):
+    print(f"[DASHBOARD] {document_name} available")
